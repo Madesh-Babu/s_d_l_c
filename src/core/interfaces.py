e1d3429c4ad8f941185f7e9eabf77fc65a758902
Madesh Babu <madeshbabu@bitcot.com>
2026-02-17 19:09:53 +0530

    updated interfaces

--- src/core/interfaces.py
+++ src/core/interfaces.py
@@ -1,9 +1,11 @@
 from abc import ABC, abstractmethod
 
+
 class IProductCreator(ABC):
     @abstractmethod
     def create_product(self, data):
         pass
+
 
 class IProductReader(ABC):
     @abstractmethod
@@ -14,17 +16,19 @@ class IProductReader(ABC):
     def get_products(self, product_id):
         pass
 
+
 class IProductUpdater(ABC):
     @abstractmethod
     def update_product(self, product_id, data):
         pass
+
 
 class IProductDeleter(ABC):
     @abstractmethod
     def delete_product(self, product_id):
         pass
 
-                                    #Category_Interface
+        # Category_Interface
 
 
 class ICategoryCreator(ABC):
@@ -32,19 +36,22 @@ class ICategoryCreator(ABC):
     def create_category(self, data):
         pass
 
+
 class ICategoryReader(ABC):
     @abstractmethod
     def get_all_categories(self):
         pass
 
     @abstractmethod
-    def get_category(self,category_id):
+    def get_category(self, category_id):
         pass
+
 
 class ICategoryUpdater(ABC):
     @abstractmethod
     def update_category(self, category_id, data):
         pass
+
 
 class ICategoryDeleter(ABC):
     @abstractmethod
